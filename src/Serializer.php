0ae72efab2094c7776e4c65ab98fbeacb5f3138f
Mathias Arlaud <mathias.arlaud@gmail.com>
2024-03-15 16:35:21 +0100

    feat: handle collections

--- src/Serializer.php
+++ src/Serializer.php
@@ -10,12 +10,15 @@ use Mtarld\JsonEncoderBundle\EncoderInterface;
 use Mtarld\JsonEncoderBundle\JsonDecoder;
 use Mtarld\JsonEncoderBundle\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\TypeInfo\Type;
 use Symfony\Component\TypeInfo\TypeResolver\StringTypeResolver;
+use Symfony\Component\TypeInfo\Type\CollectionType;
 use TurboSerializer\Exception\UnsupportedException;
 
 final readonly class Serializer implements SerializerInterface
 {
-    public const NORMALIZED_SHAPE = 'turbo_normalized_shape';
+    public const TYPE = 'type';
+    public const NORMALIZED_TYPE = 'normalized_type';
 
     /**
      * @param JsonEncoder $encoder
@@ -30,7 +33,7 @@ final readonly class Serializer implements SerializerInterface
     }
 
     /**
-     * @param array<string, mixed>&array{turbo_dto_proxy: string}
+     * @param array<string, mixed>&array{type?: Type, normalized_type?: Type}
      */
     public function serialize(mixed $data, string $format, array $context = []): string
     {
@@ -38,17 +41,28 @@ final readonly class Serializer implements SerializerInterface
             throw new UnsupportedException(sprintf('"%s" format is not supported', $format));
         }
 
-        if (array_key_exists(static::NORMALIZED_SHAPE, $context)) {
-            $data = $this->autoMapper->map($data, $context[static::NORMALIZED_SHAPE]);
+        $type = $context[static::TYPE] ?? (\is_object($data) ? Type::object($data::class) : Type::builtin(get_debug_type($data)));
 
-            return (string) $this->encoder->encode($data, config: $context + ['type' => $this->typeResolver->resolve($context[static::NORMALIZED_SHAPE])]);
+        if (!array_key_exists(static::NORMALIZED_TYPE, $context)) {
+            return (string) $this->encoder->encode($data, $type, $context);
         }
 
-        return (string) $this->encoder->encode($data, $context);
+        $normalizedType = $context[static::NORMALIZED_TYPE];
+
+        if ($normalizedType instanceof CollectionType) {
+            $normalized = [];
+            foreach ($data as $item) {
+                $normalized[] = $this->autoMapper->map($item, (string) $normalizedType->getCollectionValueType(), $context);
+            }
+
+            return (string) $this->encoder->encode($normalized, $normalizedType, $context);
+        }
+
+        return (string) $this->encoder->encode($this->autoMapper->map($data, (string) $normalizedType, $context), $normalizedType, $context);
     }
 
     /**
-     * @param array<string, mixed>&array{turbo_dto_proxy: string}
+     * @param array<string, mixed>&array{normalized_type?: Type}
      */
     public function deserialize(mixed $data, string $type, string $format, array $context = []): mixed
     {
@@ -56,12 +70,24 @@ final readonly class Serializer implements SerializerInterface
             throw new UnsupportedException(sprintf('"%s" format is not supported', $format));
         }
 
-        if (array_key_exists(static::NORMALIZED_SHAPE, $context)) {
-            $proxy = $this->decoder->decode($data, $this->typeResolver->resolve($context[static::NORMALIZED_SHAPE], $context));
+        $type = $this->typeResolver->resolve($type);
 
-            return $this->autoMapper->map($proxy, $type, $context);
+        if (!array_key_exists(static::NORMALIZED_TYPE, $context)) {
+            return $this->decoder->decode($data, $type, $context);
         }
 
-        return $this->decoder->decode($data, $this->typeResolver->resolve($type), $context);
+        $normalizedType = $context[static::NORMALIZED_TYPE];
+        $decoded = $this->decoder->decode($data, $normalizedType, $context);
+
+        if ($normalizedType instanceof CollectionType) {
+            $denormalized = [];
+            foreach ($decoded as $item) {
+                $denormalized[] = $this->autoMapper->map($item, (string) $normalizedType->getCollectionValueType(), $context);
+            }
+
+            return $denormalized;
+        }
+
+        return $this->autoMapper->map($decoded, (string) $type, $context);
     }
 }
