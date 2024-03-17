076f63dc3def1a7b9b7c47afda3dbade76519cdb
Mathias Arlaud <mathias.arlaud@gmail.com>
2024-03-17 11:57:05 +0100

    fixes

--- config/services.php
+++ config/services.php
@@ -3,14 +3,9 @@
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use AutoMapper\AutoMapperInterface;
-use Symfony\Component\Serializer\SerializerInterface;
 use TurboSerializer\Serializer;
 
 return static function (ContainerConfigurator $container) {
-    $container->services()->defaults()
-        ->bind(SerializerInterface::class. '$turboSerializer', service('turbo_serializer'))
-    ;
-
     $container->services()
         ->set('turbo_serializer', Serializer::class)
         ->args([
